849afa16121d5b61b4cbcc9b431e734186ac7e06
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-12-15 20:26:03 -0800

    efficiencies

--- readMatrix.cpp
+++ readMatrix.cpp
@@ -33,13 +33,3 @@ for(int i = 0; i < size; ++i)
         }
     }
 }
-
-//// initializing a 2-d array size 2
-//int minutes[2][2] = {{0,0},
-//                     {1,1}};
-//
-//// initializing a 2-D array size 3
-//int hours[3][3] = {{1,1,1},
-//                   {2,2,2},
-//                   {3,3,3}};
-//
