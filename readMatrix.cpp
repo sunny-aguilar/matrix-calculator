df7e4a450be0b331c1a5a0be04892ce0c8f0b165
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-06 14:38:40 -0800

    lab1

--- readMatrix.cpp
+++ readMatrix.cpp
@@ -14,25 +14,25 @@ using std::endl;
 
 void readMatrix(int **matrixPtr, int size) {
 
-for(int i = 0; i < size; ++i)
-    // allocate arrays based on size value
-    matrixPtr[i] = new int[size];
+    for(int i = 0; i < size; ++i) {
+        // allocate arrays based on size value
+        matrixPtr[i] = new int[size];
+    }
 
-    // ask user to enter integers for matrix
-    cout << "Now, enter 4 integers sizes 0 - 9.\n";
-    for(int row = 0; row < size; ++row) {
-        for(int col = 0; col < size; ++col) {
-            cout << "\nEnter a Number: ";
-            cin >> matrixPtr[row][col];
-
-
-            // validate input
-            while(!cin || (matrixPtr[row][col] < -9 || matrixPtr[row][col] > 9)) {
-                cout << "Error! Enter an integer between 0 and 9: ";
-                cin.clear();                // clear error flag in cin
-                cin.ignore();               // ignore user input
+        // ask user to enter integers for matrix
+        cout << "Now, enter 4 integers sizes 0 - 9.\n";
+        for(int row = 0; row < size; ++row) {
+            for(int col = 0; col < size; ++col) {
+                cout << "\nEnter a Number: ";
                 cin >> matrixPtr[row][col];
+
+                // validate input
+                while(!cin || (matrixPtr[row][col] < -9 || matrixPtr[row][col] > 9)) {
+                    cout << "Error! Enter an integer between 0 and 9: ";
+                    cin.clear();                // clear error flag in cin
+                    cin.ignore();               // ignore user input
+                    cin >> matrixPtr[row][col];
+                }
             }
         }
-    }
 }
