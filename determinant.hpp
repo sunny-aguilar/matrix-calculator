01879d3e0b7376c4e06c93a4d3b46e4f18fa10f3
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-01-06 15:46:53 -0800

    lab1

--- determinant.hpp
+++ determinant.hpp
@@ -1,14 +1,14 @@
 /*********************************************************************
 ** Author:          Sandro Aguilar
 ** Date:            Jan 2019
-** Description:     Write a short description
-**                  of your code
-**                  here.
-**
+** Description:     A function that calculates the determinant of a
+**                  2x2 or 3x3 matrix calculator. Returns the
+**                  calculated value.
 *********************************************************************/
 #ifndef DETERMINANT_HPP
 #define DETERMINANT_HPP
 
+// function prototype
 int determinant(int **matrixPtr, int size);
 
 
--- readMatrix.cpp
+++ readMatrix.cpp
@@ -2,9 +2,8 @@
 ** Author:          Sandro Aguilar
 ** Date:            Jan 2019
 ** Description:     A function that take a pointer to a 2-D array and
-**                  an int as parameters. The function promps the user
+**                  an int as parameters. The function prompts the user
 **                  to enter the values for a size 2 or size 3 matrix.
-**
 *********************************************************************/
 #include "readMatrix.hpp"
 #include <iostream>
